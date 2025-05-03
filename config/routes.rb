2194ff0fb11cdca95bd1385be935a0d0319eb9eb
NoFoster <kfoster0@chicagobooth.edu>
2025-05-03 16:59:54 +0000

    Final

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "items", :action => "index" })
+
+  post("/make_a_new_item", { :controller => "items", :action => "create_item" })
+
+  get("/backdoor", { :controller => "items", :action => "show_display_form" })
 end
